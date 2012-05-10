b81e39448224fdd09887e67cce88ed5e6458bfb5
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-10 17:18:17 -0300

    added some classes

--- templates/fjm_turina_landing_page.tpl.php
+++ templates/fjm_turina_landing_page.tpl.php
@@ -16,19 +16,19 @@
     <div class="front-postcard"></div>
   </div>
   <div class="front-wrapper-bottom">
-    <div class="front-content">
+    <div class="front-content col-1">
       <h2 class="front-title"><?php print t('Content'); ?></h2>
       <?php print $content; ?>
     </div>
-    <div class="front-collections">
+    <div class="front-collections col-1">
       <h2 class="front-title"><?php print t('Collections'); ?></h2>
       <?php print $collections; ?>
     </div>
-    <div class="front-browse">
+    <div class="front-browse col-1">
       <h2 class="front-title"><?php print t('Browse'); ?></h2>
       <?php print $browse; ?>
     </div>
-    <div class="front-search">
+    <div class="front-search col-2">
       <h2 class="front-title"><?php print t('Search'); ?></h2>
       <?php print $search; ?>
     </div>
