256a80af335ac8cc80beed60c26bbdde6d6020d4
Danny Joris <danny_joris@yahoo.co.uk>
2012-05-11 10:57:17 -0300

    wrapper div for landing page blocks

--- templates/fjm_turina_landing_page.tpl.php
+++ templates/fjm_turina_landing_page.tpl.php
@@ -16,21 +16,23 @@
     <div class="front-postcard"></div>
   </div>
   <div class="front-wrapper-bottom">
-    <div class="front-content col-1">
-      <h2 class="front-title"><?php print t('Content'); ?></h2>
-      <?php print $content; ?>
-    </div>
-    <div class="front-collections col-1">
-      <h2 class="front-title"><?php print t('Collections'); ?></h2>
-      <?php print $collections; ?>
-    </div>
-    <div class="front-browse col-1">
-      <h2 class="front-title"><?php print t('Browse'); ?></h2>
-      <?php print $browse; ?>
-    </div>
-    <div class="front-search col-2">
-      <h2 class="front-title"><?php print t('Search'); ?></h2>
-      <?php print $search; ?>
+    <div class="front-wrapper-bottom-inner">
+      <div class="front-content col-1">
+        <h2 class="front-title"><?php print t('Content'); ?></h2>
+        <?php print $content; ?>
+      </div>
+      <div class="front-collections col-1">
+        <h2 class="front-title"><?php print t('Collections'); ?></h2>
+        <?php print $collections; ?>
+      </div>
+      <div class="front-browse col-1">
+        <h2 class="front-title"><?php print t('Browse'); ?></h2>
+        <?php print $browse; ?>
+      </div>
+      <div class="front-search col-2">
+        <h2 class="front-title"><?php print t('Search'); ?></h2>
+        <?php print $search; ?>
+      </div>
     </div>
   </div>
 </div>
