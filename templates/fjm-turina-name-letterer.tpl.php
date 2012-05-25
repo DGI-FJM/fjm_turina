8d2c5be793bc951a5539d61745c6365c0096cda4
Adam Vessey <adam.vessey@gmail.com>
2012-05-25 21:05:46 +0200

    Add browse pages.

--- templates/fjm-turina-name-letterer.tpl.php
+++ templates/fjm-turina-name-letterer.tpl.php
@@ -1,21 +1,25 @@
 <?php ?>
 <ul class="turina-letterer">
-  <li>
-    <a href="<?php echo url('names');?>" class="letter"><?php echo t('ALL');?></a>
-<?php foreach ($queries as $query => $count):?>
-  <li>
-  <?php 
+  <li class="<?php
+  $class = 'letter';
+  if (t('ALL') == $selected):
+    $class .= ' selected';
+  endif;
+  echo $class;?>">
+    <a href="<?php echo url($path);?>"><?php echo t('ALL');?></a>
+<?php foreach ($queries as $query => $count):
   $disp = $map[$query];
   $class = 'letter';
   if ($disp == $selected):
     $class .= ' selected';
-  endif;
-  
+  endif;?>
+  <li class="<?php echo $class; ?>">
+<?php 
   if ($count > 0):?>
-    <a href="<?php echo url("names/$disp");?>" class="<?php echo $class; ?>"><?php echo $disp ;?></a>
-<?php else:?>
-    <span class="<?php echo $class; ?>"><?php echo $disp; ?></span>
-<?php endif;?>
+    <a href="<?php echo url("$path/$disp");?>"><?php echo $disp ;?></a>
+<?php else:
+      echo $disp;
+    endif;?>
   </li>
 <?php endforeach;?>
 </ul>
--- templates/fjm-turina-name-list.tpl.php
+++ templates/fjm-turina-name-list.tpl.php
@@ -1,5 +1,5 @@
-<ul class="turina-name-list">
+<ul class="turina-facets">
   <?php foreach ($names as $name): ?>
-  <li><?php echo l($name, "islandora/solr/search/*:*/$field:\"$name\"")?></li>
+  <li><?php echo l(truncate_utf8($name, 72, TRUE, TRUE), "islandora/solr/search/*:*/$field:\"$name\"")?></li>
   <?php endforeach; ?>
 </ul>
